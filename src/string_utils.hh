696195d75ed989b8748048efabc213f30d140619
Tanguy Segarra <tanguy.segarra@epita.fr>
2020-07-20 18:10:08 +0200

    utils: add trim and csv formatting string functions

---
+++ src/string_utils.hh
@@ -0,0 +1,8 @@
+#pragma once
+
+std::string ltrim(const std::string& s);
+std::string rtrim(const std::string& s);
+std::string trim(const std::string& s);
+std::string format_csv(std::string& s);
+
+#include "string_utils.hxx"
---
+++ src/string_utils.hxx
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <regex>
+#include <string>
+#include <vector>
+
+std::string ltrim(const std::string& s)
+{
+    return std::regex_replace(s, std::regex("^\\s+"), std::string(""));
+}
+
+std::string rtrim(const std::string& s)
+{
+    return std::regex_replace(s, std::regex("\\s+$"), std::string(""));
+}
+
+std::string trim(const std::string& s)
+{
+    return ltrim(rtrim(s));
+}
+
+std::string format_csv(std::string& s)
+{
+    s = trim(s);
+    std::replace(s.begin(), s.end(), ' ', '_');
+    return s;
+}
