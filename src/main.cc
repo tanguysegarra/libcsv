4554c943b04fbee312558849cddddc7f9aac7f4f
Tanguy Segarra <tanguy.segarra@epita.fr>
2020-07-20 18:10:26 +0200

    main: update main example

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,22 @@
 int main(void)
 {
     auto r = csv::Reader("src/test.csv");
-    r.display();
+    auto colnames = r.get_column_names();
+    for (size_t i = 0; i < colnames.size(); i++)
+    {
+        std::cout << colnames[i];
+        if (i != colnames.size() - 1)
+            std::cout << " | ";
+    }
+    std::cout << "\n";
+
+    for (auto& colname : colnames)
+    {
+        std::cout << colname << ": ";
+        for (auto& val : r[colname])
+            std::cout << val << ", ";
+        std::cout << "\n";
+    }
 
     return 0;
 }
