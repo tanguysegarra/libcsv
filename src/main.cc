fce4697309a3f54bd56496f1f64d81f02634f260
Tanguy Segarra <tanguy.segarra@epita.fr>
2020-07-21 11:52:42 +0200

    main: update main with writer example

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,10 @@
+//#include "csv.hh"
 #include "reader.hh"
+#include "writer.hh"
 
 int main(void)
 {
+    /* TESTING READER CLASS */ 
     auto r = csv::Reader("src/test.csv");
     auto colnames = r.get_column_names();
     for (size_t i = 0; i < colnames.size(); i++)
@@ -19,6 +22,19 @@ int main(void)
             std::cout << val << ", ";
         std::cout << "\n";
     }
+
+    for (auto& val : r["NAME"])
+        std::cout << val << ", ";
+
+    std::cout << "\n";
+
+    /* TESTING WRITER CLASS */ 
+
+    auto data = r.get_rows();
+
+    auto w = csv::Writer("output.csv");
+    w.set_rows(data);
+    w.write_rows();
 
     return 0;
 }
